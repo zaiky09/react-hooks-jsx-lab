a7697c93fa77e0f10417a38ff4d43ac8fa75d868
zaiky09 <zamilmozamil@outlook.com>
2023-01-11 19:05:18 +0300

    tests passing

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,16 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
+
+  const divStyle ={
+    color: 'firebrick'
+  };
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id="home">
+      <h1 style={divStyle}>{name} is a Web Developer from {city}</h1>
+    </div>
+  );
 }
 
 export default Home;
